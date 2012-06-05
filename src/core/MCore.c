c2ce78e039ba9035a70129ba480d3250b30fd019
Mariusz Słonina <mariusz.slonina@gmail.com>
2012-06-05 11:48:09 +0200

    Missing basic docs for Prepare/Process() hooks

--- src/core/MCore.c
+++ src/core/MCore.c
@@ -4,10 +4,18 @@
  */
 #include "MCore.h"
 
+/**
+ * @function
+ * The Welcome message
+ */
 void Welcome() {
   Message(MESSAGE_INFO, "This is Mechanic2.\n");
 }
 
+/**
+ * @function
+ * The Prepare hook
+ */
 int Prepare(module *m) {
   int mstat = 0;
   setup *s = &(m->layer.setup);
@@ -21,6 +29,10 @@ int Prepare(module *m) {
   return mstat;
 }
 
+/**
+ * @function
+ * The Process hook
+ */
 int Process(module *m) {
   int mstat = 0;
   setup *s = &(m->layer.setup);
