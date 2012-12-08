2032cc619968991c23866aabc270e7c690eac479
Mariusz Słonina <mariusz.slonina@gmail.com>
2012-12-08 14:45:57 +0100

    Initial examples for Node/Loop hooks

---
+++ examples/mechanic_module_ex_loop.c
@@ -0,0 +1,15 @@
+#include "Mechanic2.h"
+
+/**
+ * Implements LoopPrepare()
+ */
+int LoopPrepare(int mpi_size, int node, pool **all, pool *p, setup *s) {
+  return SUCCESS;
+} 
+
+/**
+ * Implements LoopProcess()
+ */
+int LoopProcess(int mpi_size, int node, pool **all, pool *p, setup *s) {
+  return SUCCESS;
+} 
---
+++ examples/mechanic_module_ex_node.c
@@ -0,0 +1,15 @@
+#include "Mechanic2.h"
+
+/**
+ * Implements NodePrepare()
+ */
+int NodePrepare(int mpi_size, int node, pool **all, pool *p, setup *s) {
+  return SUCCESS;
+} 
+
+/**
+ * Implements NodeProcess()
+ */
+int NodeProcess(int mpi_size, int node, pool **all, pool *p, setup *s) {
+  return SUCCESS;
+} 
