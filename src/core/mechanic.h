0757b6ee3d79ef7b0565de59f1e1f4f96bdd9500
Mariusz Słonina <mariusz.slonina@gmail.com>
2012-12-21 00:41:57 +0100

    One step closer to the task intermediate storage

--- src/core/mechanic.h
+++ src/core/mechanic.h
@@ -72,6 +72,10 @@
 #define TASK_IN_USE -1 /**< The task in use return code */
 #define TASK_TO_BE_RESTARTED -2 /**< The task to be restarted return code */
 #define NO_MORE_TASKS -99 /**< No more tasks return code */
+#define TASK_FINALIZE 3001 /**< The task finalize return code */
+#define TASK_RESET 3002 /**< The task return return code */
+#define TASK_CREATE_NEW 3003 /**< The task create new return code */
+#define TASK_CHECKPOINT 3004 /**< The task checkpoint return code */
 
 /* Message tags */
 #define TAG_DATA 1337 /**< Send/Receiving data tag */
@@ -250,7 +254,10 @@ typedef struct {
 typedef struct {
   int pid; /**< The parent pool id */
   int tid; /**< The task id */
+  int rid; /**< The task reset id */
+  int cid; /**< The task checkpoint id */
   int status; /**< The task status */
+  int state; /**< The task processing state */
   int location[TASK_BOARD_RANK]; /**< Coordinates of the task */
   short node; /** The computing node */
   storage *storage; /**< The storage schema and data */
