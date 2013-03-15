f6a6191204438670705c285141bc3783dc28a2a0
Mariusz Slonina <slonina@astri.umk.pl>
2013-03-15 11:13:42 +0100

    More functional unit testing with external cmake script

--- tests/test.cmake
+++ tests/test.cmake
@@ -1,1 +1,12 @@
-execute_process(COMMAND ${TEST} RESULT_VAR HAD_ERROR)
+set (ENV{LD_LIBRARY_PATH} ".")
+set (ENV{DYLD_LIBRARY_PATH} ".")
+
+execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} OUTPUT_VARIABLE
+  TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+
+if (EOUT) 
+  message(STATUS ${TOUT})
+  message(STATUS ${ROUT})
+  message(FATAL_ERROR ${EOUT})
+endif (EOUT)
+
