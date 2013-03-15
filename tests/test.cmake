7f9fed7bf01683df0fc252f1d07af88f6ba23034
Mariusz Slonina <slonina@astri.umk.pl>
2013-03-15 14:22:23 +0100

    Added more unit tests

--- tests/test.cmake
+++ tests/test.cmake
@@ -14,9 +14,12 @@ if (EOUT)
   message(FATAL_ERROR ${EOUT})
 endif (EOUT)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -DSOURCEDIR=${CMAKE_CURRENT_SOURCE_DIR} -P
-  ${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}.cmake)
-execute_process(COMMAND diff a.txt r.txt
+#execute_process(COMMAND ${CMAKE_COMMAND} -DSOURCEDIR=${CMAKE_CURRENT_SOURCE_DIR} -P
+#  ${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}.cmake)
+#execute_process(COMMAND diff a.txt r.txt
+#  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+
+execute_process(COMMAND h5diff ${MODULE}-master-00.h5 references/${MODULE}-master-00.h5
   OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
 
 if (TOUT)
@@ -26,23 +29,21 @@ endif (TOUT)
 #
 # The restart mode
 #
-message(STATUS "Testing restart mode")
-execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} -x 10 -y 10 -b 3 -d 13 
-  --restart-mode --restart-file=${MODULE}-master-02.h5
-  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
-
-if (EOUT) 
-  message(STATUS ${TOUT})
-  message(STATUS ${ROUT})
-  message(FATAL_ERROR ${EOUT})
-endif (EOUT)
-
-execute_process(COMMAND ${CMAKE_COMMAND} -DSOURCEDIR=${CMAKE_CURRENT_SOURCE_DIR} -P
-  ${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}.cmake)
-execute_process(COMMAND diff a.txt r.txt
-  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
-
-if (TOUT)
-  message(FATAL_ERROR ${TOUT})
-endif (TOUT)
+#message(STATUS "Testing restart mode")
+#execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} -x 10 -y 10 -b 3 -d 13 
+#  --restart-mode --restart-file=${MODULE}-master-02.h5
+#  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+#
+#if (EOUT) 
+#  message(STATUS ${TOUT})
+#  message(STATUS ${ROUT})
+#  message(FATAL_ERROR ${EOUT})
+#endif (EOUT)
+#
+#execute_process(COMMAND h5diff ${MODULE}-master-00.h5 references/${MODULE}-master-00.h5
+#  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+#
+#if (TOUT)
+#  message(FATAL_ERROR ${TOUT})
+#endif (TOUT)
 
