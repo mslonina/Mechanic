036e417998d328d45c9f3903074d28dbed2bfb97
Mariusz Slonina <slonina@astri.umk.pl>
2013-03-15 13:26:18 +0100

    Added restart mode test coverage

--- tests/test.cmake
+++ tests/test.cmake
@@ -1,8 +1,12 @@
 set (ENV{LD_LIBRARY_PATH} ".")
 set (ENV{DYLD_LIBRARY_PATH} ".")
 
-execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} OUTPUT_VARIABLE
-  TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+#
+# The normal mode
+#
+message(STATUS "Testing normal mode")
+execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} -x 10 -y 10 -b 3 -d 13 
+  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
 
 if (EOUT) 
   message(STATUS ${TOUT})
@@ -18,3 +22,27 @@ execute_process(COMMAND diff a.txt r.txt
 if (TOUT)
   message(FATAL_ERROR ${TOUT})
 endif (TOUT)
+
+#
+# The restart mode
+#
+message(STATUS "Testing restart mode")
+execute_process(COMMAND mpirun -np 4 mechanic -p ${MODULE} -n ${MODULE} -x 10 -y 10 -b 3 -d 13 
+  --restart-mode --restart-file=${MODULE}-master-02.h5
+  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+
+if (EOUT) 
+  message(STATUS ${TOUT})
+  message(STATUS ${ROUT})
+  message(FATAL_ERROR ${EOUT})
+endif (EOUT)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -DSOURCEDIR=${CMAKE_CURRENT_SOURCE_DIR} -P
+  ${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}.cmake)
+execute_process(COMMAND diff a.txt r.txt
+  OUTPUT_VARIABLE TOUT RESULT_VARIABLE ROUT ERROR_VARIABLE EOUT)
+
+if (TOUT)
+  message(FATAL_ERROR ${TOUT})
+endif (TOUT)
+
